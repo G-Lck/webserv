0db71d8b9dc5c68d2f0031dfb4733b5ab89aaf17
G Lucken <glucken@Gs-MacBook-Air.local>
2026-06-23 13:47:27 +0200

    create httprequest class

--- includes/HttpRequest.hpp
+++ includes/HttpRequest.hpp
@@ -1,15 +1,29 @@
 #ifndef HTTPREQUEST_HPP
-# define HTTPREQUEST_HPP
+#define HTTPREQUEST_HPP
 
-#include "WebServ.hpp"
+#include <string>
 
-class HttpRequest
-{
+class HttpRequest {
+
 	private:
-		/* data */
+
+		std::string method; // GET POST...
+		std::string path; // /home/images/...
+		std::string version; // HTTP//1.1
+		std::map<std::string, std::string> headers;
+		std::string body;
+
+		void parseRequestLine(const std::string& line);
+		void parseHeaders(const std::string& raw);
+		bool parseBody(const std::string& raw);
+
 	public:
+
 		HttpRequest();
-		~HttpRequest();
+		HttpRequesrt(const HttpRequest& other);
+		HttpRequesrt& operator=(const HttpRequesrt& other);
+		~HttpRequesrt();
+		bool parse(const std::string& raw);
 };
 
 #endif
